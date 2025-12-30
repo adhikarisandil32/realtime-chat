554dfb04bcac1bfadbfee1ccc2fddf6cbeb3a88e
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-12-31 02:26:12 +0545

    api query and query keys

---
+++ frontend/src/services/apis/chats.ts
@@ -0,0 +1,23 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { AxiosRequestConfig } from "axios";
+import { api } from "../axios-instance";
+import { queryKeys } from "@/utils/react-query-keys";
+
+export const useChats = <T = any>(config?: AxiosRequestConfig) => {
+  return useSuspenseQuery<T>({
+    queryKey: [queryKeys.chats],
+    queryFn: async () => {
+      // return await api.get("/products", config);
+      return await new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          try {
+            const response = await api.get("/api/chats", config);
+            resolve(response as T);
+          } catch (error) {
+            reject(error);
+          }
+        }, 10 * 1000);
+      });
+    },
+  });
+};
