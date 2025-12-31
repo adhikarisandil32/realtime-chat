8400c8b48c4ad0794f58e7da920ba22851ecaddb
Sandil Adhikari <adhikarisandil32@gmail.com>
2026-01-01 03:27:14 +0545

    move out history fetching and client list show logic

--- frontend/src/components/socket-chat.tsx
+++ frontend/src/components/socket-chat.tsx
@@ -1,8 +1,10 @@
 import { useSocket } from "@/providers/socket-provider";
 import { clearLocal } from "@/utils/local-storage";
-import { useEffect, useRef, useState, type FormEvent } from "react";
+import { Suspense, useEffect, useRef, useState, type FormEvent } from "react";
 import ClientCount from "./client-counts";
-import { useChats } from "@/services/api/chats";
+import { LoaderCircle } from "lucide-react";
+import ChatHistoryBox from "./chat-history-box";
+import Loading from "./loading-animation";
 
 interface SocketConnectionProps {
   username: string;
@@ -16,17 +18,9 @@ interface IConversation {
 
 function SocketChat({ username }: SocketConnectionProps) {
   const inputMessageRef = useRef<HTMLInputElement | null>(null);
-  const latestConversationRef = useRef<HTMLDivElement | null>(null);
   const [conversations, setConversations] = useState<IConversation[]>([]);
 
   const { socket, connectedClients } = useSocket();
-
-  useEffect(() => {
-    latestConversationRef.current?.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-    });
-  }, [conversations]);
 
   useEffect(() => {
     const messageListener = (data: IConversation) => {
@@ -47,7 +41,7 @@ function SocketChat({ username }: SocketConnectionProps) {
 
     const message = inputMessageRef.current.value;
 
-    socket?.emit("message", { sender: username, message });
+    socket.emit("message", { sender: username, message });
     inputMessageRef.current.value = "";
   };
 
@@ -61,24 +55,9 @@ function SocketChat({ username }: SocketConnectionProps) {
           />
 
           <div className="my-1 px-2 space-y-1 grid grid-cols-6">
-            {conversations.map((conversation, idx, initialArr) => (
-              <div
-                key={`${conversation.sender}-${conversation.createdAt}`}
-                className={`px-2 py-1 rounded-lg ${
-                  conversation.sender === username
-                    ? "bg-blue-800 text-gray-100 col-start-2 col-end-7"
-                    : "border border-black col-start-1 col-end-6"
-                }`}
-                ref={
-                  idx + 1 === initialArr.length ? latestConversationRef : null
-                }
-              >
-                <p className="font-semibold truncate">
-                  {conversation.sender}:{" "}
-                </p>
-                <p>{conversation.message}</p>
-              </div>
-            ))}
+            <Suspense fallback={<Loading />}>
+              <ChatHistoryBox username={username} />
+            </Suspense>
           </div>
         </div>
 
@@ -90,26 +69,26 @@ function SocketChat({ username }: SocketConnectionProps) {
             ref={inputMessageRef}
             type="text"
             placeholder="Enter message"
-            className="border border-black rounded-sm w-full px-1"
+            className="border border-black rounded-sm w-full py-1 px-2"
           />
           <div className="flex justify-between w-full">
-            <button
-              type="submit"
-              className="border border-black rounded-sm px-4 w-fit"
-            >
-              Send
-            </button>
-
             <button
               type="button"
               className="text-gray-100 bg-red-600 rounded-sm px-4 w-fit"
               onClick={() => {
-                socket?.disconnect();
+                socket.disconnect();
                 clearLocal();
                 window.location.reload();
               }}
             >
               Exit
+            </button>
+
+            <button
+              type="submit"
+              className="border border-black rounded-sm px-4 w-fit"
+            >
+              Send
             </button>
           </div>
         </form>
