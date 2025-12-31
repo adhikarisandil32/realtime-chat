802b65631e71a2b307d6677c0ccbbdac552e970e
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-12-31 18:08:09 +0545

    add socket provider

---
+++ frontend/src/providers/socket-provider.tsx
@@ -0,0 +1,52 @@
+import { envConfig } from "@/config/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { io, type Socket } from "socket.io-client";
+
+interface ISocketContext {
+  socket: Socket;
+  connectedClients: number;
+}
+
+const SocketContext = React.createContext<ISocketContext | undefined>(
+  undefined
+);
+
+export const useSocket = () => {
+  const context = React.useContext(SocketContext);
+
+  if (!context) {
+    throw new Error("user context within the provider");
+  }
+
+  return context;
+};
+
+const socket = io(envConfig.apiUrl, { autoConnect: false });
+const queryClient = new QueryClient();
+
+export default function SocketProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [connectedClients, setConnectedClients] = useState(0);
+
+  useEffect(() => {
+    const countListener = (count: number) => setConnectedClients(count);
+
+    socket.connect();
+    socket.on("client-count", countListener);
+
+    return () => {
+      socket.disconnect();
+      socket.off("client-count");
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, connectedClients }}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SocketContext.Provider>
+  );
+}
