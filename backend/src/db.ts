9235cef26d0162a3dbbe7fb335d0ccb835097552
Sandil Adhikari <adhikarisandil32@gmail.com>
2026-01-01 21:26:14 +0545

    change chat message format with status and id

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,9 +2,11 @@ import { JSONFilePreset } from "lowdb/node";
 import path from "path";
 
 interface IChat {
+  id: string;
   sender: string;
   message: string;
   createdAt: number;
+  status: "sent" | "failed" | "pending";
 }
 
 interface IData {
