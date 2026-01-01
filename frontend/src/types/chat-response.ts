eccc73967aaed2afc6de06e738890abbc88f6bdb
Sandil Adhikari <adhikarisandil32@gmail.com>
2026-01-02 00:45:26 +0545

    update chat response and client-side generated chat message

--- frontend/src/types/chat-response.ts
+++ frontend/src/types/chat-response.ts
@@ -1,5 +1,11 @@
 export interface IChatResponse {
+  id: string;
   message: string;
   sender: string;
   createdAt: number;
+  status: "sent" | "failed" | "pending";
+}
+
+export interface IClientChat extends Omit<IChatResponse, "id"> {
+  identifier: string;
 }
