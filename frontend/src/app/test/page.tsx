59c3bc7a73c593a1cff96f8bde8457f488d1ed28
Sandil Adhikari <adhikarisandil32@gmail.com>
2026-01-09 00:05:39 +0545

    import fixes

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
+import ProviderChildOne from "./_components/provider-child-1";
 import ProviderChildTwo from "./_components/provider-child-2";
-import ProvChildComp from "./_components/provider-child-component";
 import ProviderComponent from "./_components/provider-component";
 
 export default function TestPage() {
   return (
     <ProviderComponent>
-      <ProvChildComp />
+      <ProviderChildOne />
       <ProviderChildTwo />
     </ProviderComponent>
   );
