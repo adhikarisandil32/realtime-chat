7accd7cd26471a1d6b9d483f404196d01c6be0ad
Sandil Adhikari <adhikarisandil32@gmail.com>
2026-01-12 02:33:17 +0545

    indicate typing for more than one user

--- frontend/src/components/typing-indictor.tsx
+++ frontend/src/components/typing-indictor.tsx
@@ -3,19 +3,24 @@ import { useEffect, useState } from "react";
 
 export default function TypingIndicator({ className }: { className?: string }) {
   const { socket } = useSocketGetters();
-  const [isTyping, setIsTyping] = useState({ state: false, user: null });
+  const [isTyping, setIsTyping] = useState({ state: false, users: [] });
 
   useEffect(() => {
-    socket.on("typing-on", (user) => setIsTyping({ state: true, user }));
-    socket.on("typing-off", (user) => setIsTyping({ state: false, user }));
+    socket.on("typing-on", (users) => setIsTyping({ state: true, users }));
+    socket.on("typing-off", (users) => setIsTyping({ state: false, users }));
 
     return () => {
       socket.off("typing-on");
       socket.off("typing-off");
     };
-  });
+  }, []);
+
+  const top3TypingUsers =
+    isTyping.users.length > 2
+      ? `${isTyping.users[0]}, ${isTyping.users[1]} and more typing...`
+      : `${isTyping.users.join(" and ")} typing...`;
 
   return isTyping.state ? (
-    <p className={className}>({isTyping.user} is typing...)</p>
+    <p className={className}>({top3TypingUsers})</p>
   ) : null;
 }
